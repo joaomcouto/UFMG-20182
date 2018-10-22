ff6372af86298f873c063cc0dc60ff56f1ce7b04
Roberto Rosmaninho <robertogrosmaninho@outlook.com>
2018-10-22 16:01:38 -0300

    adding superclass character

---
+++ character.h
@@ -0,0 +1,25 @@
+#ifndef PDS2_CHARACTER_H
+#define PDS2_CHARACTER_H
+
+#include "string"
+
+class Character{
+private: 
+ std::string _name;
+ int _level;
+ int _hp;
+ int _mp;
+ std::string _type;
+ int _hability;
+
+public:
+ Character();
+ ~Character();
+
+ std::string getName();
+ int getLevel();
+ int getHP();
+ int getMP();
+ std::string getType();
+}
+#endif
