31dfb24c73cbb0ecbbf9258c021b39fdebcb8c64
joaomcouto <33331313+joaomcouto@users.noreply.github.com>
2018-11-03 19:16:47 -0300

    Update character.cpp

--- character.cpp
+++ character.cpp
@@ -1,9 +1,8 @@
 #include "character.h"
 #include <string>
 
-Character::Character(std::string name, int level, int hp, int mp,
-std::string type, int skill) : _name(name), _level(level), _hp(hp), _mp(mp), 
-_type(type), _skill(skill) {};
+Character::Character(std::string name, std::string type, int level, int hp, int mp, int str, int cons, int dex) : 
+_name(name),_type(type), _level(level), _hp(hp), _mp(mp),  _strenght(str), _constitution(cons), _dexterity(dex) {};
 
 Character::~Character(){}
 
@@ -23,10 +22,20 @@ int Character::getMP(){
 	return this->_mp;
 }
 
-int Character::getSkill(){
-	return this->_skill;
+
+int Character::getStrenght(){
+	return this->_strenght ; 
+}
+
+int Character::getConstitution(){
+	return this->_constitution ;
+}
+
+int Character::getDexterity(){
+	return this->_dexterity ;
 }
 
 std::string Character::getType(){
 	return this->_type; 
 }
+
