5fd4b1e89cf81ba2d72ef0750bb1d4cabf129ced
isadorasalles <alves.isa99@gmail.com>
2018-11-06 08:02:31 -0200

    game.h

---
+++ game.h
@@ -0,0 +1,20 @@
+#ifndef GAME_H
+#define GAME_H
+
+//#include "character.h"
+//#include "wizard.h"
+//#include "spells.h"
+//#include "objects.h"
+//#include "inventory.h"
+
+#include <string>
+#include <vector>
+
+void texts1();
+std::string choice_of_wand();
+std::string choice_of_house();
+void initialize_player(std::string wand, std::string house);
+void texts2();
+
+
+#endif
