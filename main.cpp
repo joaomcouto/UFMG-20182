c2e4d909518533c5201cbc016c03d7cd957d1f7e
Roberto Rosmaninho <robertogrosmaninho@outlook.com>
2018-11-14 14:31:21 -0200

    fix bugs

--- main.cpp
+++ main.cpp
@@ -9,11 +9,11 @@ int main (){
 	choice_of_house();
 	Inventory *inventory = new Inventory();
 	std::vector<Objeto*> object = inventory->instantiate_object();
-	std::vector<Feiticos*> spell = inventory->instantiate_spell();
+	std::vector<Spell*> spell = inventory->instantiate_spell();
 	inventory->print_object(object);
 	inventory->set_object(object, 1);
 	inventory->print_spells(spell);
-	
+
 	for (auto Objeto : object)
 		delete Objeto;
 	delete inventory;
