4dbc21d89206800a4542ddf8fb58b16a64af7cd2
Roberto Rosmaninho <robertogrosmaninho@outlook.com>
2018-11-17 21:52:35 -0200

    add new tests

--- tests/test_wizard.cpp
+++ tests/test_wizard.cpp
@@ -7,3 +7,22 @@ TEST_CASE("Testing Construtor"){
     std::vector<Artifacts *> _artifacts;
     CHECK_NOTHROW(Wizard *Harry = new Wizard("Harry Potter", 1, 100, 100, 1, 1, 1, "Gryffindor", "Holly wood and Phoenix feathers", "Deer", _spells, _potions, _artifacts));
 }
+
+
+TEST_CASE("Testing SkillPoint - Getters and Setters"){
+		std::vector<Spell *> _spells;
+    std::vector<Potions *> _potions;
+    std::vector<Artifacts *> _artifacts;		
+    Wizard *Harry = new Wizard("Harry Potter", 1, 100, 100, 1, 1, 1, "Gryffindor", "Holly wood and Phoenix feathers", "Deer", _spells, _potions, _artifacts);	
+		
+		int _skillPoints = Harry->getSkillPoints();
+			CHECK(_skillPoints == 3);
+	
+		Harry->incrementSkillPoints(2);
+		_skillPoints = Harry->getSkillPoints();
+			CHECK(_skillPoints == 5);
+
+		Harry->incrementSkill(1);
+		_skillPoints = Harry->getSkillPoints();
+			CHECK(_skillPoints == 4);
+}
