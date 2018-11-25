6ddf167683d81bd4be22ff9e776386c0cb492c41
Robertorosmaninho <robertogrosmaninho@outlook.com>
2018-11-25 16:50:19 -0200

    fix stats netive

--- include/stats.h
+++ include/stats.h
@@ -14,20 +14,30 @@ typedef struct Stats {
 	inline Stats operator +=(Stats a){
 		this->level += a.level ;
 		this->hp += a.hp ;
+			if(this->hp < 0) {this->hp = 0;}
 		this->mp += a.mp ;
+			 if(this->mp < 0) {this->mp = 0;}
 		this->strenght += a.strenght ;
+				 if(this->strenght < 0) {this->strenght = 0;}
 		this->constitution += a.constitution ;
+				 if(this->constitution < 0) {this->constitution = 0;}
 		this->dexterity += a.dexterity ;
+				 if(this->dexterity < 0) {this->dexterity = 0;}
 		return *this ;
 	}
 	inline Stats operator +(Stats a){
 		Stats t ;
 		t.level = this->level + a.level ;
 		t.hp = this->hp + a.hp ;
+			if(t.hp < 0) {t.hp = 0;}
 		t.mp = this->mp + a.mp ;
+			if(t.mp < 0) {t.mp = 0;}
 		t.strenght =this->strenght + a.strenght ;
+			if(t.strenght < 0) {t.strenght = 0;}
 		t.constitution =this->constitution + a.constitution ;
+			if(t.constitution < 0) {t.constitution = 0;}
 		t.dexterity =this->dexterity + a.dexterity ;
+			if(t.dexterity < 0) {t.dexterity = 0;}
 		return t ;
 	}
 	void printStats(){
