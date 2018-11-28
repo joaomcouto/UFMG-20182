b936be981226a0147bebeeb6b228621b389087e0
Roberto Rosmaninho <robertogrosmaninho@outlook.com>
2018-11-28 21:11:14 -0200

    add game test

--- tests/test_game.cpp
+++ tests/test_game.cpp
@@ -1,8 +1,9 @@
 #include "doctest.h"
 #include "game.h"
+#include "wizard.h"
 
-TEST_CASE("Testing Text"){
-
-CHECK_NOTHROW(texts1());
-
+TEST_CASE("Testing Throws"){
+	CHECK_NOTHROW(texts1());
+//	CHECK_NOTHROW(initialize_game());   ---> Isso dá errado
 }
+
