e240e0566bc731bb8c51b0942329f44f3525385d
isadorasalles <alves.isa99@gmail.com>
2018-11-29 07:17:21 -0200

    Alguns testes a mais

--- tests/test_game.cpp
+++ tests/test_game.cpp
@@ -4,6 +4,10 @@
 
 TEST_CASE("Testing Throws"){
 	CHECK_NOTHROW(texts1());
+	CHECK_NOTHROW(instantiate_spell());
+	CHECK_NOTHROW(instantiate_artifacts());
+	CHECK_NOTHROW(instantiate_attack());
+	CHECK_NOTHROW(instantiate_potions());
 //	CHECK_NOTHROW(initialize_game());   ---> Isso dá errado
 }
 
